b1e50b9d54480e4138539b62a217a97c76146984
khavelka20 <khavelka20@gmail.com>
2016-05-18 20:23:57 -0400

    a

--- app/Services/Steam/SteamService.php
+++ app/Services/Steam/SteamService.php
@@ -4,16 +4,18 @@ namespace App\Services;
 
 class SteamService {
     
-    private static $apiKey = "8F451AE3DC1F264AF6BADC449E9D1419";
-    private static $apiBaseUrl = "http://api.steampowered.com/";
-    private static $userPrefix = "ISteamUser";
-    private static $playerSummarryPrefix = "GetPlayerSummaries";
-    private static $apiVersion = "v0002";
+    private $apiKey = "8F451AE3DC1F264AF6BADC449E9D1419";
+    private $apiBaseUrl = "http://api.steampowered.com/";
+    private $userPrefix = "ISteamUser";
+    private $playerSummarryPrefix = "GetPlayerSummaries";
+    private $apiVersion = "v0002";
     
     public function getSteamPlayerSummary($steamId) 
     {
-        $url = this::$apiBaseUrl . "/" . this::$userPrefix . "/" . this::$playerSummarryPrefix . "/" . this::$apiVersion . "/?key=" . this::$apiKey . "&steamids=" . $steamId;
-        echo $json = file_get_contents($url);
+        $url = $this->apiBaseUrl . "/" . $this->userPrefix . "/" . $this->playerSummarryPrefix . "/" . 
+                $this->apiVersion . "/?key=" . $this->apiKey . "&steamids=" . $steamId;
+        
+        return $json = file_get_contents($url);
     }
 
 }
